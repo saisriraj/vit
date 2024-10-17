1640e5aed289edb8dd479b845168945044b56735
sri raj <saisriraj.5@gmail.com>
2024-10-17 04:19:49 -0500

    f

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.2",
+    author="sri",
+    author_email="saisriraj.5@gmail.com",
+    packages=find_packages()
+)
